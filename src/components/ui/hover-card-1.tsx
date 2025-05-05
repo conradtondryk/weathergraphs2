88f7176a3efaaf39e834b2b873432ae712aa662d
Your Name <konradtondryk@gmail.com>
2025-05-06 00:29:00 +0100

    hover card function add

--- src/components/ui/hover-card-1.tsx
+++ src/components/ui/hover-card-1.tsx
@@ -5,19 +5,26 @@ import {
 } from "@/components/hover-card";
 import { Button } from "@/components/ui/button";
 import { CalendarIcon } from "lucide-react";
+import Link from "next/link";
+
+const portfolioLink = (
+  <Link href="https://portfolio.ctondryk.dev" className="text-blue-500">
+    portfolio.ctondryk.dev
+  </Link>
+);
 
 export function HoverCard1() {
   return (
     <HoverCard>
       <HoverCardTrigger asChild>
-        <Button variant="link">@nextjs</Button>
+        <Button variant="link">@ctondryk.dev</Button>
       </HoverCardTrigger>
       <HoverCardContent className="w-80">
         <div className="flex justify-between space-x-4">
           <div className="space-y-1">
-            <h4 className="text-sm font-semibold">@nextjs</h4>
+            <h4 className="text-sm font-semibold">@ctondryk.dev</h4>
             <p className="text-sm">
-              The React Framework – created and maintained by @vercel.
+              A project for my portfolio, view my other work at {portfolioLink}
             </p>
             <div className="flex items-center pt-2">
               <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
