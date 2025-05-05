6153aa87cf5f017e7ddb402ff488f120e0e92dbf
Your Name <konradtondryk@gmail.com>
2025-05-05 02:45:48 +0100

    create graphtype button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import GraphType from "@/components/ui/graphtype";
 import { ThemeProvider } from "@/components/theme-provider";
 import ModeToggle from "@/components/ui/themebutton";
 
--- src/components/ui/graphtype.tsx
+++ src/components/ui/graphtype.tsx
@@ -1,2 +1,44 @@
+"use client";
+
 import { ChartColumnBig } from "lucide-react";
 import { ChartLine } from "lucide-react";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function GraphType() {
+  const { graphType, setGraphType } = setGraphType();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  if (graphType === "column") {
+    return (
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => setGraphType("line")}
+      >
+        <ChartColumnBig className="h-[1.2rem] w-[1.2rem] transition-all hover:cursor-pointer" />
+      </Button>
+    );
+  }
+
+  if (graphType === "line") {
+    return (
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => setGraphType("column")}
+      >
+        <ChartLine className="h-[1.2rem] w-[1.2rem] transition-all hover:cursor-pointer" />
+      </Button>
+    );
+  }
+}
