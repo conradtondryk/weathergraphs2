49a9f169c6f180545782b58ca002275e41b87d21
Your Name <konradtondryk@gmail.com>
2025-05-06 00:34:54 +0100

    style hover card

--- src/components/ui/hover-card-1.tsx
+++ src/components/ui/hover-card-1.tsx
@@ -4,12 +4,14 @@ import {
   HoverCardTrigger,
 } from "@/components/hover-card";
 import { Button } from "@/components/ui/button";
-import { CalendarIcon } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
 const portfolioLink = (
   <Link href="https://portfolio.ctondryk.dev" className="text-blue-500">
-    portfolio.ctondryk.dev
+    <span className="flex items-center">
+      portfolio.ctondryk.dev <ArrowUpRight className="h-4 w-4 opacity-70" />
+    </span>
   </Link>
 );
 
@@ -24,12 +26,12 @@ export function HoverCard1() {
           <div className="space-y-1">
             <h4 className="text-sm font-semibold">@ctondryk.dev</h4>
             <p className="text-sm">
-              A project for my portfolio, view my other work at {portfolioLink}
+              A weather app built for my portfolio, making use of API calls and
+              graphing.
             </p>
             <div className="flex items-center pt-2">
-              <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
               <span className="text-xs text-muted-foreground">
-                Joined December 2021
+                {portfolioLink}{" "}
               </span>
             </div>
           </div>
