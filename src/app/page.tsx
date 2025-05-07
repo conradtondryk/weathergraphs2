5aa47b98f1d652e90f3922a651d5da05928ce061
Your Name <konradtondryk@gmail.com>
2025-05-07 01:54:06 +0100

    prepare carousel for api functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Graph2 from "@/components/graph2";
-import TemperatureGraph from "@/components/TemperatureGraph";
+import Graph2 from "@/components/graph-2";
+import TemperatureGraph from "@/components/Temperature-Graph";
+import { Carousel1 } from "@/components/ui/carousel-1";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 max-w-4xl mx-auto">
       <TemperatureGraph />
       <Graph2 />
+      <Carousel1 />
     </main>
   );
 }
