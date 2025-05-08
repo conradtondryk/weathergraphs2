90afcc18e7125971c101f92547a1948fdedeae44
Your Name <konradtondryk@gmail.com>
2025-05-08 01:05:11 +0100

    create carousel slides

--- src/components/ui/carousel-1.tsx
+++ src/components/ui/carousel-1.tsx
@@ -15,13 +15,12 @@ export function Carousel1() {
   return (
     <Carousel className="w-full max-w-xs">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card></Card>
-            </div>
-          </CarouselItem>
-        ))}
+        <CarouselItem key={index}>
+          <div className="p-1">
+            <Card></Card>
+          </div>
+        </CarouselItem>
+        ))
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
