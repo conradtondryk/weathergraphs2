abfd8023989073e9ee8270cdb3e4329c861266aa
Your Name <konradtondryk@gmail.com>
2025-05-10 00:23:43 +0100

    remove old carousel imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Graph2 from "@/components/graph-2";
 import TemperatureGraph from "@/components/temperature-graph1";
-import { Carousel1 } from "@/components/ui/carousel-1";
 import { SparkAreaChart } from "@/tremorcomponents/spark-chart";
 import { WeatherData } from "@/lib/weather-data";
 
@@ -9,7 +8,6 @@ export default function Home() {
     <main className="flex flex-col gap-4 max-w-4xl mx-auto">
       <TemperatureGraph />
       <Graph2 />
-      <Carousel1 />
     </main>
   );
 }
