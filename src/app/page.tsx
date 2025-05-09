7433376e5bbbdebcec971f3aa24892a3a2b11817
Your Name <konradtondryk@gmail.com>
2025-05-10 00:25:37 +0100

    add carousel demo to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Graph2 from "@/components/graph-2";
 import TemperatureGraph from "@/components/temperature-graph1";
 import { SparkAreaChart } from "@/tremorcomponents/spark-chart";
 import { WeatherData } from "@/lib/weather-data";
+import { CarouselDemo } from "@/components/carousel-demo";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 max-w-4xl mx-auto">
       <TemperatureGraph />
+      <CarouselDemo />
       <Graph2 />
     </main>
   );
