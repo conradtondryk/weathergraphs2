13a8c9f579b85e12d84cbfb9d6330c06c5aa8170
Your Name <konradtondryk@gmail.com>
2025-06-18 23:05:31 +0100

    create interface

--- src/lib/weather-data.ts
+++ src/lib/weather-data.ts
@@ -1,9 +1,28 @@
-export interface WeatherData {
-  date: string;
-  temperature_c: number;
-  humidity_percent: number;
+interface RawData {
+  time: string[];
+  uv_index_max: number[];
+  temperature_2m_max: number[];
+  temperature_2m_min: number[];
+  precipitation_sum: number[];
+  wind_speed_10m_max: number[];
+}
+
+interface WeatherData {
+  time: string;
   uv_index: number;
-  precipitation_mm: number;
-  wind_speed_kmh: number;
-  id: string;
+  temperature_max: number;
+  temperature_min: number;
+  precipitation: number;
+  wind: number;
+} 
+
+function transformWeatherData(data: RawData): WeatherData[] {
+  return data.time.map((time, index) => ({
+    time: time,
+    uv_index: data.uv_index_max[index],
+    temperature_max: data.temperature_2m_max[index],
+    temperature_min: data.temperature_2m_min[index],
+    precipitation: data.precipitation_sum[index],
+    wind: data.wind_speed_10m_max[index],
+  }));
 }
